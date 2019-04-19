6e30f177a9cdf3521d9abfeeab7e47a75d05ab90
dpenkdcha <dpenkdcha@gmail.com>
2019-04-19 18:11:25 +0530

    Add Days to Date

--- src/AddDays.java
+++ src/AddDays.java
@@ -4,8 +4,8 @@ public class AddDays {
 
 	     public static void main(String []args){
 	        System.out.println("Hello World");
-	        String date = "01032000";
-	        int addDays = 2254;
+	        String date = "19112019";
+	        int addDays = 2000;
 	        
 	        HashMap<Integer, Integer> hm = new HashMap<>();
 	        hm.put(1, 31);
@@ -33,45 +33,54 @@ public class AddDays {
 	        int yearsToAdd = 0;
 	        int monthsToAdd = 0;
 	        int daysToAdd = 0;
-	        int leapYearInBtwn = 0;
 	        int extraDays = 0;
-	        int finalYear = 0;
+	        int finalYear = Integer.parseInt(datearr[2]);;
 	        int finalMonth = 0;
 	        int finalDay = 0;
 	        int year = Integer.parseInt(datearr[2]);
+	        int month = Integer.parseInt(datearr[1]);
+	        int day = Integer.parseInt(datearr[0]);
 	        
-	            
 	        yearsToAdd = addDays / 365;
-        	leapYearInBtwn = yearsToAdd/4;
-        	if((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)) && Integer.parseInt(datearr[1]) <=2)
-        		extraDays = (addDays % 365) - leapYearInBtwn - 1;
-        	else
-        		extraDays = (addDays % 365) - leapYearInBtwn;
-        	System.out.println("Years to be added: " + yearsToAdd + " Leap Year Included: " + leapYearInBtwn + " Extra Days: " + extraDays);
+	        extraDays = (addDays % 365);
+	        
+	        if(yearsToAdd >= 1) {
+	        	for(int i = 0; i<yearsToAdd; i++) {
+	        		finalYear = finalYear + 1;
+	        		if((finalYear % 400 == 0) || ((finalYear % 4 == 0) && (finalYear % 100 != 0))) {
+	        			extraDays = extraDays-1;
+	        		}
+	        	}
+	        }
+        
+	        if(((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) && Integer.parseInt(datearr[1]) <= 2) {
+	        	extraDays = extraDays-1;
+	        }
+	        
+        	System.out.println(" Leap Year Included: " + extraDays + " Calculated year : " + finalYear);
         	
-            finalYear = Integer.parseInt(datearr[2]) + yearsToAdd;
-            System.out.println("Calculated year : " + finalYear);
-            
             //for months 
             System.out.println("Current Month: " + Integer.parseInt(datearr[1]));
         
-            for(int i=Integer.parseInt(datearr[1]); (i<=12 && extraDays >= hm.get(i)); i++) {
-        		extraDays = extraDays - hm.get(i);
-        		monthsToAdd++;
-        		
-            	if((finalYear % 400 == 0) || ((finalYear % 4 == 0) && (finalYear % 100 != 0)) && monthsToAdd == 2 && finalYear != Integer.parseInt(datearr[2])) {
-            		System.out.println("Current is Leap");
-            		extraDays = extraDays - 1;
-        		}
-            	System.out.println("Days in month " + i + " : " + hm.get(i) + " Remaining Days " + extraDays + " Add in month : " + monthsToAdd);
-            }
-            finalMonth  = Integer.parseInt(datearr[1]) + monthsToAdd;
-            System.out.println("Calculated Month :" + finalMonth);
             
-            finalDay = Integer.parseInt(datearr[0]) + extraDays;
+            extraDays = day + extraDays;
+            for(int i=1; (i<12 && extraDays >= hm.get(month)); i++) {
+        		extraDays = extraDays - hm.get(month);
+        		System.out.println("Days in month " + month + " : " + hm.get(month) + " Remaining Days " + extraDays);
+        		if(month < 12) {
+        			month++;
+        		} else {
+        			month = 1;
+        			finalYear++;
+        		}
+        		System.out.println(" Add in month : " + month);
+            }
+            System.out.println("Calculated Month :" + month);
+            
+            finalDay = extraDays;
             System.out.println("Calculated Day :" + finalDay);
             
-            String finalDate = finalDay+""+finalMonth+""+finalYear;
+            String finalDate = finalDay+""+month+""+finalYear;
             System.out.println("Calculated Date :" + finalDate);
 	            
 	        
